77f23f300f074ee0af6f2506b3bbbd2e5df3f25b
Tiago Angelo <kurtis.angelo@gmail.com>
2025-11-10 20:48:26 -0300

    Implement OpenTelemetry in controller reconcilers

--- internal/controller/cluster/workspace/workspace.go
+++ internal/controller/cluster/workspace/workspace.go
@@ -142,9 +142,9 @@ func Setup(mgr ctrl.Manager, o controller.Options, timeout, pollJitter time.Dura
 		return err
 	}
 
-	r := managed.NewReconciler(mgr,
+	r := otel.NewInstrumentedReconciler(managed.NewReconciler(mgr,
 		resource.ManagedKind(v1beta1.WorkspaceGroupVersionKind),
-		opts...)
+		opts...))
 
 	return ctrl.NewControllerManagedBy(mgr).
 		Named(name).
